222051559d94915c1638503421b82f17b6799aac
nyghtowl <warrick.melanie@gmail.com>
2014-03-16 23:13:18 -0700

    Added ipython notebook for now to protect content. I did get gmaillib to work on loading gmails. Will test out a second package to compare which ones provide best results. Added gmail login variables to config to pull values from local env.

--- config.py
+++ config.py
@@ -15,5 +15,7 @@ TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 TWILIO_NUM = os.environ.get('TWILIO_NUM')
 TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID')
 MY_NUM = os.environ.get('MY_NUM')
+TEST_GMAIL_ID = os.environ.get('test_gmail_id_2')
+TEST_GMAIL_PWD = os.environ.get('test_gmailpwd_2')
 
 twilio_client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
