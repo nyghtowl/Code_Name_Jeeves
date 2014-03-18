b204c158c872df7820f0cbb27265123571c9b1a2
nyghtowl <warrick.melanie@gmail.com>
2014-03-18 10:50:43 -0700

    Added target column to database structure. Also adjusted some of the comments and variable names to make it clear what the data is and how its being passed. Also moved functions around and grouped based on building the table vs pulling and storing the data. Will probabily reuse creating and dropping the tables.

--- app/email_model.py
+++ app/email_model.py
@@ -3,8 +3,23 @@ import gmail
 import datetime
 
 '''
-Creating table for a data dump
+Creating and storing raw(ish) data.
 '''
+
+# Building the structure
+
+table_data = {
+                "raw_data": [
+                "message_id varchar(255) not null,  thread_id varchar(255), \
+                to_email varchar(255), \
+                from_email varchar(255), \
+                cc varchar(255), \
+                date timestamp, \
+                subject varchar(255), \
+                body text, \
+                target boolean, \
+                PRIMARY KEY(message_id)", "message_id"]
+            }
 
 def create_table(table_name, values):
     db.execute('''create table %s(%s)''' % (table_name, values))
@@ -12,42 +27,41 @@ def create_table(table_name, values):
 def create_index(table_name, idx_col):
     db.execute('''create index id_%s on %s (%s);''' % (table_name, table_name, idx_col)) 
 
-def build_tables():
+def drop_table(name):
+    db.execute('''DROP TABLE IF EXISTS %s;''' % (name))
+
+def build_tables(table_data):
     # Use nvarchar in case storing non-english data
-    table_data = {
-                    "mw_prof_emails": ["message_id varchar(255) not null,  thread_id varchar(255), to_email varchar(255), from_email varchar(255), cc varchar(255), date timestamp,subject varchar(255), body text, PRIMARY KEY(message_id)", "message_id"]
-                }
 
     for idx, (name, params) in enumerate(table_data.iteritems()):
         create_table(name, params[0])
         create_index(name, params[1])
 
-def drop_table(name):
-    db.execute('''DROP TABLE IF EXISTS %s;''' % (name))
+# Getting and Storing the Data
 
 def clean_body(body):
     return body.replace('\r', '').replace('\xa9', '')
 
-    # replace('\n', ' ')
-
 def store_email(email):
+    target = False
     store_e = '''
         insert into mw_prof_emails 
         (message_id, thread_id, to_email, from_email, cc, date, subject, body)
-        values (%s, %s, %s, %s, %s, %s, %s, %s);
+        values (%s, %s, %s, %s, %s, %s, %s, %s, %s);
     '''
     # in psycopg - var placeholder must be %s even with int or dates or other types
-    db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, clean_body(email.body)))
+    if 'Jeeves' in email.labels and '\\Starred' in email.labels:
+        target = True
+    db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, email.labels, clean_body(email.body), target))
 
-# may be able to pull from other folders
 def get_emails():
     gmail_conn = gmail.login(TEST_GMAIL_ID, TEST_GMAIL_PWD)
     emails = gmail_conn.inbox().mail(after=datetime.date(2014, 3, 1))
     for email in emails:
-        email.fetch()
-        store_email(email)
+        # Call fetch to pull in email attributes
+        store_email(email.fetch())
 
     gmail_conn.logout()
-    print "Check logged out.", gmail_conn.logged_in
+    print "Logged in?", gmail_conn.logged_in
 
 # conn.close()
