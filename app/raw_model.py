029999163e94792bc5d00d06349f66db53a9c37d
nyghtowl <warrick.melanie@gmail.com>
2014-03-18 16:31:42 -0700

    Lots o changes. Was in that debugging zone so not many compact commits on this one. Main changes were reworking the structure to allow for enough storage on certain fields, also added target field to leverage specific email tags for what does classify as true. Also reworked the data loading functions to try to abstract them further such as using label method in gmail to call different types of boxes. And had to do some cleanup on the message esp for string literals. After a lot of searching finally found the regex for the string literal to strip that out of the message body before storing. Last note is that I setup data storage so that it will try except skip emails that were already loaded. Thus allows to keep loading emails that are not stored and skip ones already loaded vs having to figure out which ones to load.

--- app/raw_model.py
+++ app/raw_model.py
@@ -1,25 +1,15 @@
 from config import conn, db, TEST_GMAIL_ID, TEST_GMAIL_PWD
+import psycopg2
 import gmail
 import datetime
+import re
+import pdb
 
 '''
 Creating and storing raw(ish) data.
 '''
 
 # Building the structure
-
-table_data = {
-                "raw_data": [
-                "message_id varchar(255) not null,  thread_id varchar(255), \
-                to_email varchar(255), \
-                from_email varchar(255), \
-                cc varchar(255), \
-                date timestamp, \
-                subject varchar(255), \
-                body text, \
-                target boolean, \
-                PRIMARY KEY(message_id)", "message_id"]
-            }
 
 def create_table(table_name, values):
     db.execute('''create table %s(%s)''' % (table_name, values))
@@ -40,28 +30,63 @@ def build_tables(table_data):
 # Getting and Storing the Data
 
 def clean_body(body):
-    return body.replace('\r', '').replace('\xa9', '')
+    # sample string literal issues found 0xc2, 0x20, \xa9, 0x80
+    if body:
+        body = body.replace('\r', '').replace('\n', ' ')
+        body = re.sub(r'[\x80-\xff]', r'', body)
+        body = re.sub(r'\s+', ' ', body)
+    return body
 
 def store_email(email):
-    target = False
+    target, starred = False, False
     store_e = '''
-        insert into mw_prof_emails 
-        (message_id, thread_id, to_email, from_email, cc, date, subject, body)
-        values (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+        insert into raw_data 
+        (message_id, thread_id, to_email, from_email, cc, date, subject, starred, body, target)
+        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     '''
     # in psycopg - var placeholder must be %s even with int or dates or other types
-    if 'Jeeves' in email.labels and '\\Starred' in email.labels:
-        target = True
-    db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, email.labels, clean_body(email.body), target))
 
-def get_emails():
+    if 'Jeeves' in email.labels and '\\Starred' in email.labels:
+        target = starred = True
+    elif '\\Starred' in email.labels:
+        starred = True
+
+    try:
+        db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, starred, clean_body(email.body), target))
+    except psycopg2.IntegrityError:
+    # if exists then skip loading it
+        pass
+
+def get_emails(box, date=datetime.datetime.now()):
     gmail_conn = gmail.login(TEST_GMAIL_ID, TEST_GMAIL_PWD)
-    emails = gmail_conn.inbox().mail(after=datetime.date(2014, 3, 1))
+    emails = gmail_conn.label(box).mail(after=date)
+
+    print "num emails:", len(emails)
     for email in emails:
         # Call fetch to pull in email attributes
-        store_email(email.fetch())
+        email.fetch()
+        store_email(email)
 
     gmail_conn.logout()
     print "Logged in?", gmail_conn.logged_in
 
+def main():
+    table_data = {
+                "raw_data": [
+                "message_id varchar(255) not null, \
+                thread_id varchar(255), \
+                to_email text, \
+                from_email varchar(255), \
+                cc varchar(255), \
+                date timestamp, \
+                subject text, \
+                starred boolean, \
+                body text, \
+                target boolean, \
+                PRIMARY KEY(message_id)", "message_id"]
+            }
+    build_tables(table_data)
+    get_emails('INBOX', datetime.date(2014, 3, 16))
+    get_emails('Career')
+    get_emails('Hackday_Group')
 # conn.close()
