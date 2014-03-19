3262d20e8f23ad936e43af658b4aa5a766dd1c9e
nyghtowl <warrick.melanie@gmail.com>
2014-03-18 22:35:30 -0700

    Reworked file to focus on pulling in data into pandas from postgres. Had to overcome getting science lib to work in virtualenv. Worked through creating a virtual connection back to anaconda and then just ended up reloading the packages because the depth of the current directory made it too difficult for the system to search out any associated tools. Also determiend that vanilla pandas will load psql data vs teh special pandas.sql.io because it throughs an error about psycopg2 not having rollbacks.

--- app/data_eda.py
+++ app/data_eda.py
@@ -1,36 +1,51 @@
 '''
-Jeeves Project
-
-Main Working File
+Data EDA file - using pandas to look at data
 
 '''
+from config import conn, db
 
 import pandas as pd
 import numpy as np
 import seaborn
-import vincent
-vincent.core.initialize_notebook()
+# import vincent
+# vincent.core.initialize_notebook()
 
 import matplotlib.pyplot as plt
 
-from sklearn import decomposition
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import LogisticRegression
-
 from time import time
 
+# Load data from sql
 
-import gmaillib
+def load_data():
+    db.execute("SELECT to_email, subject from raw_data")
+    return pd.DataFrame(db.fetchall(), columns=['to_email', 'subject'])
 
-account = gmaillib.account('username', 'password') # initialize
-account.inbox(start=0, amount=10) # get messages from inbox and just a range
-#account.get_all_messages() # alternative
-#account.get_inbox_count()
-#account.filter('from:foo@bar.com') - pass email that can be filtered
+
+# Explore Data
+
+def eda(data):
+    print "Summary Stats", data.describe()
+    print "Shape", data.shape
+    print "Top 5 rows", data.head()
+    print "Bottom 5 rows", data.tail()
+    print "Missing Values", ((data.shape[0] * data.shape[1]) - data.count().sum())
+
+def plot_hist(data, bin=10):
+    #range(min,max+binwidth,binwidth)
+    plt.hist(data, bins=bin)
+
+def print_cloud(data):
+    # Good for analyzing bag of words
+    for i in range(data.shape[0]):
+        word_dict = {feature_names[idx]: data[i][idx]*100 for idx in data[i].argsort()[:-20:-1]}
+        cloud = vincent.Word(word_dict)
+        print "Cloud", i
+        cloud.display()
+
 
 def main():
-    pass
+    df = load_data()
+    return df
 
 if __name__ == '__main__':
     main()
