12bb23745a8648b13a73c04d5137ff5600480c3d
nyghtowl <warrick.melanie@gmail.com>
2014-03-20 09:40:56 -0700

    Started adding PCA code. There is opportunity to condense this down and abstract with nmf.

--- app/feature_model.py
+++ app/feature_model.py
@@ -18,7 +18,7 @@ from time import time
 def create_vectorizer(vectorizer_object):
     # tokens = nltk.word_tokenize(article)
     #vectorizer = vectorizer_object(min_df=2, strip_accents='unicode', max_features=5000, analyzer='word',token_pattern=r'\\w{1,}',ngram_range=(1, 2))
-    vectorizer = vectorizer_object(min_df=2, strip_accents='unicode', max_features=5000, analyzer='word',ngram_range=(1, 1))
+    vectorizer = vectorizer_object(min_df=2, strip_accents='unicode', max_features=10000, analyzer='word',ngram_range=(1, 1), stop_words='english')
     return vectorizer
 
 def create_bag(features, vectorizer):
@@ -36,10 +36,12 @@ def fit_nmf(k, bag):
     print "done in %0.3fs." % (start - time())
     return nmf  
 
-def nmf_transform(nmf):
-    W = nmf.transform(bag_tf) # apps by k
+def nmf_transform(nmf, bag):
+    start = time()
+    W = nmf.transform(bag) # apps by k
     H = nmf.components_ # words by k
     print W.shape, H.shape
+    print "done in %0.3fs." % (start - time())
     return W, H
 
 def top_vals(matrix, labels, num):
@@ -55,6 +57,35 @@ def review_nmf(H, W, feature_names, data):
     print top_vals(W.T, data, 3)
 
 
+# Feature Decomposition - PCA
+
+def fit_pca():
+
+    pca = decomposition.RandomizedPCA(n_components=2)
+    return pca
+
+def transform_pca(pca):
+    x_pca = pca.fit_transform(x_train)
+    return x_pca
+
+def plot_scatter(x_pca, y_train):
+    colors = ['r', 'b']
+    markers = ['o', 's']
+    plt.figure(figsize=(20,10))
+
+    plt.xlim(-0.05, 0.45)
+    plt.ylim(-0.2, 0.8)
+    for i, c, m in zip(np.unique(y_train), colors, markers):
+        plt.scatter(x_pca[y_train == i, 0], 
+                    x_pca[y_train == i, 1],
+                    c=c, 
+                    marker=m, 
+                    label=i, 
+                    alpha=0.7, 
+                    s=200)
+
+    _ = plt.legend(loc='best')
+
 # def main():
 #     # cv = create_vectorizer(CountVectorizer)
 #     tf = create_vectorizer(TfidfVectorizer)
@@ -64,7 +95,6 @@ def review_nmf(H, W, feature_names, data):
 
     # nmf = fit_nmf(7, bag.todense())
 
-# Feature Decomposition - PCA
 
 # Feature Decomposition - SVD
 
