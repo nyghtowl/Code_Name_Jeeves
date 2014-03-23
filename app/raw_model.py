e55d50fc7bf329f613879920c948a5b387188c75
nyghtowl <warrick.melanie@gmail.com>
2014-03-23 00:02:02 -0700

    Pulled uneeded code out. Consolidated certain functions that are regularly used into one script called common.

--- app/raw_model.py
+++ app/raw_model.py
@@ -1,5 +1,8 @@
 '''
-Creating and storing raw(ish) email data.
+Creating Postgres Structures
+
+Storing raw(ish) email data
+
 '''
 
 from config import conn, db
@@ -7,7 +10,7 @@ import psycopg2
 import datetime
 import re
 import pdb
-import fetch_gmail
+import app.common as cpm
 
 
 # Building the structure
@@ -28,15 +31,7 @@ def build_tables(table_data):
         create_table(name, params[0])
         create_index(name, params[1])
 
-# Getting and Storing the Data
-
-def clean_body(body):
-    # sample string literal issues found 0xc2, 0x20, \xa9, 0x80
-    if body:
-        body = body.replace('\r', '').replace('\n', ' ')
-        body = re.sub(r'[\x80-\xff]', r'', body)
-        body = re.sub(r'\s+', ' ', body)
-    return body
+# Getting and storing the raw data
 
 def store_email(email):
     target, starred = False, False
@@ -54,16 +49,14 @@ def store_email(email):
 
     # Could apply executemany with query and list of values but still need to do fetch first and apply changes
     try:
-        db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, starred, clean_body(email.body), target))
+        db.execute(store_e, (email.message_id, email.thread_id, email.to, email.fr, email.cc, email.sent_at, email.subject, starred, cpm.clean_raw_text(email.body), target))
     except psycopg2.IntegrityError:
     # if exists then skip loading it
         pass
 
-def batch_get_email(box, date=datetime.datetime.now()):
-    emails = fetch_gmail.get_emails(box, date)
+def store_postgres(box, date=datetime.datetime.now()):
+    emails = cpm.get_emails(box, date)
     for email in emails:
-        # Call fetch to pull in email attributes
-        email.fetch()
         store_email(email)
 
 def main():
@@ -82,9 +75,9 @@ def main():
                 PRIMARY KEY(message_id)", "message_id"]
             }
     build_tables(table_data)
-    batch_get_emails('INBOX', datetime.date(2013, 5, 1))
-    batch_get_emails('Career', datetime.date(2013, 5, 1))
-    batch_get_emails('Hackday_Group', datetime.date(2013, 5, 1))
+    store_database('INBOX', datetime.date(2013, 5, 1))
+    store_database('Career', datetime.date(2013, 5, 1))
+    store_database('Hackday_Group', datetime.date(2013, 5, 1))
 # conn.close()
 
 if __name__ == '__main__':
