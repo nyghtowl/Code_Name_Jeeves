1d576ca0713a1219f6467b2f92da7dd3aaaae649
nyghtowl <warrick.melanie@gmail.com>
2014-03-24 23:46:49 -0700

    Applied optimizaton approach to improve how to open and close the db connection with context manager.

--- app/common.py
+++ app/common.py
@@ -3,13 +3,15 @@ Common Project Methods
 
 '''
 
-from config import TEST_GMAIL_ID, TEST_GMAIL_PWD, conn, db
+from config import TEST_GMAIL_ID, TEST_GMAIL_PWD, connect_db
 import pandas
 import datetime
 import gmail
 import re
 
 import cPickle # same as pickle but faster and implemented in C
+
+# Context manager for context - with database 
 
 def pickle(stuff, filename):
     # use pkl for the filename and write in binary
@@ -39,6 +41,8 @@ def get_emails(box, date=datetime.datetime.now()):
 def clean_raw_txt(text):
     # sample string literal issues found 0xc2, 0x20, \xa9, 0x80
     if text:
+        # Consider dropping everything after On since its a repeat <(On).+>
+        text = re.sub(r'(On)\s\S+\s\S+\s\S+\s\S+\s\S+\s\S+\s(PM|AM)(,)', r'', text)
         text = text.replace('\r', '').replace('\n', ' ')
         text = re.sub(r'[\x80-\xff]', r'', text)
         text = re.sub(r'\s+', ' ', text)
@@ -47,8 +51,9 @@ def clean_raw_txt(text):
 # Load data from sql to pandas - add limit if there is too much data or only want to look at subset
 
 def load_data(table):
-    db.execute('''SELECT * from %s''' % table)
-    return pandas.DataFrame(db.fetchall(), columns=['message_id', 'thread_id', 'to_email', 'from_email', 'cc', 'date', 'subject', 'starred', 'body', 'target'])
+    with connect_db() as db:
+        db.execute('''SELECT * from %s''' % table)
+        return pandas.DataFrame(db.fetchall(), columns=['message_id', 'thread_id', 'to_email', 'from_email', 'cc', 'date', 'subject', 'starred', 'body', 'target'])
 
 def load_emails_pd():
     index_col = 'message_id'
@@ -56,6 +61,7 @@ def load_emails_pd():
     raw_df = raw_df.set_index(index_col, drop=True, verify_integrity=True)
     raw_df['weekday'] = raw_df['date'].apply(lambda d: d.weekday())
     raw_df['body'] = raw_df['body'].fillna(value='empty')
+    #    print data['body'].isnull().sum()
     return raw_df
 
 if __name__ == '__main__':
