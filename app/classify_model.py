3df8f67be0381acc648c803e9e128f7e547b195c
nyghtowl <warrick.melanie@gmail.com>
2014-03-27 11:26:53 -0700

    Pulled out creating data into common file and created code in main that will reference saved data splits or create them depending on the need for the model. Reworked main to set the stage to run each model for different results. Still working on streamlining and improving script to run and compare models. Goal to get kfolds included separately from plot and grid search.

--- app/classify_model.py
+++ app/classify_model.py
@@ -5,7 +5,7 @@ Email Classification Model Testing
 
 import app.common as cpm 
 import app.feature_model as fm
-from config import pkl_dir
+from config import pkl_dir, graph_dir
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +13,7 @@ import seaborn
 
 from scipy.stats.kde import gaussian_kde
 from sklearn.preprocessing import Binarizer
-from sklearn.cross_validation import train_test_split, KFold, StratifiedKFold, cross_val_score
+from sklearn.cross_validation import KFold, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.svm import SVC
@@ -27,16 +27,6 @@ import os
 # X is features / bag of words per email
 # Y is target true and false per email
 
-def create_datasets(X, y, split_size=.30): # regularization sprint - cross val hyper
-    #X[:,np.newaxis] ?
-    return train_test_split(X, y, test_size=split_size)
-
-def print_test_train_shape(X_train, X_test, y_train, y_test):
-
-    print 'X_train:', X_train.shape
-    print 'X_test:', X_test.shape
-    print 'y_train:', y_train.shape
-    print 'y_test:', y_test.shape
 
 def build_model(model, X_train, y_train):
     start = time()
@@ -44,22 +34,15 @@ def build_model(model, X_train, y_train):
     print "Train model in %0.2fs." % (time() - start)
     return model
 
-def classify_email(model, X_test):
-    return model.predict(X_test)
 
 def model_eval(model, X_test, y_test, save=False):
     # The mean square error - how much the values can fluctuate - need for logistic
     # print('Root Mean Squared Error: %.2f' % ((np.mean((model.predict(X_test) - y_test) ** 2))**(0.5)))
 
-    # R squared / proportion of variance explained by model: 1 is perfect prediction
-
-    # These are the same as score on the model
-    # print 'Straight Accuracy Calc:', (float(([y_val == y_pred[i] for i, y_val in np.ndenumerate(y_test)]).count(True)) / len(y_pred))
-    # print 'SKLearn Prediction Accuracy Score: %.5f' % accuracy_score(y_test, y_pred) 
-
     y_pred = model.predict(X_test)
-    y_proba = model.predict_log_proba(X_test)
+    y_proba = model.predict_proba(X_test)
 
+    # R squared / proportion of variance explained by model: 1 is perfect prediction
     print 'SKLearn Accuracy Score: %.5f' % model.score(X_test, y_test)
 
     print "Classification Report:"
@@ -84,7 +67,7 @@ def plot_confusion_matrix(cm, labels, save):
     plt.xlabel('Predicted', fontsize=14)
     plt.ylabel('Actual', fontsize=14)
     if save:
-        # os.path.join(pkl_dir, ''
+        # os.path.join(graph_dir, ''
         plt.savefig('./graph_dir/cfm.png')
     plt.show()
 
@@ -103,7 +86,7 @@ def plot_roc_curve(y_test, y_proba, save):
     plt.yticks(fontsize=10)
     plt.legend(loc='lower right')
     if save:
-        # os.path.join(pkl_dir, ''
+        # os.path.join(graph_dir, ''
         plt.savefig('./graph_dir/roc_plot.png')
 
 
@@ -112,7 +95,7 @@ def plot_roc_curve(y_test, y_proba, save):
 def get_cv_scores(model, X_test, y_test):
     return cross_val_score(model, X_test, y_test, cv=3, scoring='roc_auc')
 
-def plot_cross_val(model, X_test, y_test):
+def plot_cross_val(model, X_test, y_test, save):
     cv_scores = get_cv_scores(model, X_test, y_test)
     print 'Min Score = %f\nMean Score = %f\nMax Score = %f' % \
     (cv_scores.min(), cv_scores.mean(), cv_scores.max())
@@ -127,12 +110,14 @@ def plot_cross_val(model, X_test, y_test):
            label='Median Test Score')
     plt.legend(loc='best')
     _ = plt.title('Cross Validated Test Scores Distribution')
+    if save:
+        # os.path.join(graph_dir, ''
+        plt.savefig('./graph_dir/cross_val.png')
 
 
 def grid_search(model, params, X_train, y_train,  n_jobs=1, k_fold=3):
     start = time()
     # Confirm param vals in list form otherwise grid search throws error
-    #params = {k: (v if type(v) == list else [v]) for k, v in params.items() }
 
     clf = GridSearchCV(model, params, cv=k_fold, n_jobs=n_jobs)
     clf.fit(X_train, y_train)
@@ -157,18 +142,44 @@ def rank_features(feature_names, class_model, axis=1,start_point=-20):
     for rank, feat_index in enumerate(np.argsort(class_model.coef_[0], axis=0)[start_point:], start=1):
         print '%i: %s' % (rank, feature_names[feat_index])
 
-def main(data=None, save=False, model_fn='final_model.pkl', data_fn='pd_dataframe.pkl', vec_fn='final_vec.pkl'):
+def main(model, data=None, save=False, model_fn='final_model.pkl', data_fn='pd_dataframe.pkl', vec_fn='final_vec.pkl'):
     if data is None:
         data = cpm.unpickle(os.path.join(pkl_dir, data_fn))
 
     vectorizer = cpm.unpickle(os.path.join(pkl_dir, vec_fn))
 
-    X = fm.apply_feature_vector(vectorizer, data.body)
-    y = data.target
+    if save:
+        X_raw, y = cpm.unpickle(os.path.join(pkl_dir, 'x_y_data.pkl'))
+        if X_raw.empty:
+            X_raw, y = cpm.define_x_y_data(data, True)
+        X_vect = fm.apply_feature_vector(vectorizer, X_raw)
+        X_train, X_test, y_train, y_test = cpm.create_datasets(X_vect, y, True)
+        # Current size is train = 662 and test = 284
+    else:
+        X_train, X_test, y_train, y_test = cpm.unpickle(os.path.join(pkl_dir, 'train_split.pkl'))
+  
+    # cpm.print_test_train_shape(X_train, X_test, y_train, y_test)
 
-    model_version = LogisticRegression(C=10000.0, penalty='l2', class_weight='auto', fit_intercept=True)
+
+    if model == 'RandomForestClassifier':
+    # or model == 'GradientBoostingClassifier':
+        X_train = np.nan_to_num(X_train)
+        X_test = np.nan_to_num(X_test)
+    #     X_train = X_train.todense()
+    #     X_test = X_test.todense()
+
+
+    class_model = { 
+        'LogisticRegression': LogisticRegression(C=10000.0, penalty='l2', class_weight='auto', fit_intercept=True), 
+        'MultinomialNB': MultinomialNB(alpha=0.100000, fit_prior=True),
+        'SVC': SVC(C=10000.0, kernel='linear', shrinking=True, probability=True, class_weight='auto'),
+        'RandomForestClassifier': RandomForestClassifier(n_estimators=121, criterion='entropy', bootstrap=True, oob_score=True, max_features='auto'),
+        'GradientBoostingClassifier': GradientBoostingClassifier()
+    }
     '''
+ 
     models = [LogisticRegression(), MultinomialNB(), SVC(), RandomForestClassifier(), GradientBoostingClassifier()]
+
     params = {
         'LogisticRegression': { 'penalty': ['l1','l2'], 'C': np.logspace(0, 9, 10), 'fit_intercept': [True, False], 'class_weight':['auto']},
         'MultinomialNB':  {'alpha': np.logspace(0, 6, 7), 'fit_prior':[True, False]},
@@ -177,7 +188,7 @@ def main(data=None, save=False, model_fn='final_model.pkl', data_fn='pd_datafram
         'GradientBoostingClassifier': {'learning_rate':np.logspace(0.1, 1, 5), 'n_estimators': np.arange(50, 250, 25)}
     }
 
-    params_revise = {
+    params_round_2 = {
     'LogisticRegression':{'C': np.linspace(approx_best_c/10., approx_best_c*10., 200)},
     'MultinomialNB': {'alpha': np.linspace(approx_best_alpha/10., approx_best_alpha*10., 200)},
     'SVC': {'C': np.linspace(svc_approx_params['C']/10., svc_approx_params['C']*10., 200) }
@@ -186,16 +197,26 @@ def main(data=None, save=False, model_fn='final_model.pkl', data_fn='pd_datafram
     }
     '''
 
-    X_train, X_test, y_train, y_test = create_datasets(X, y)
-    print_test_train_shape(X_train, X_test, y_train, y_test)
-    
-    model = build_model(model_version, X_train, y_train)
-    print model
+
+    # TO DO - store each model and then run against each other
+
+    print "Model:", model
+
+    # TO DO - grid search again
+    clf = build_model(class_model[model], X_train, y_train)
+
+    model_eval(clf, X_test, y_test)
+
+    # TO DO - work on making Random work with this
+
+    #rank_features(vectorizer.get_feature_names(), clf)
 
     if save:
+        print "New classifier saved."
         cpm.pickle(model, os.path.join(pkl_dir, model_fn))
 
-    return X, y, model
+    return clf
+
 
 if __name__ == '__main__':
 
