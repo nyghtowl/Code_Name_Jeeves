4d944fc49507389319b46550a3148913aa0b9136
nyghtowl <warrick.melanie@gmail.com>
2014-03-28 11:10:07 -0700

    Reworked code so X is vectorized before its stored. Also applied random_state parameter so can setup consistancy in how train/test is split.

--- app/common.py
+++ app/common.py
@@ -61,7 +61,7 @@ def clean_raw_txt(text):
 ###########################################################
 # Pandas DataFrame
 
-def load_data(table, cols):
+def _load_data(table, cols):
     with connect_db() as db:
         db.execute('''SELECT * from %s''' % table)
         return pandas.DataFrame(db.fetchall(), columns=cols)
@@ -69,7 +69,7 @@ def load_data(table, cols):
 def load_emails_pd(table, save=False, df_fn='pd_dataframe.pkl'):
     index_col = 'message_id'
     table_cols = ['message_id', 'thread_id', 'to_email', 'from_email', 'cc', 'date', 'starred', 'subject', 'body', 'sub_body', 'email_owner', 'box','target']
-    raw_df = load_data(table, table_cols)
+    raw_df = _load_data(table, table_cols)
     raw_df = raw_df.set_index(index_col, drop=True, verify_integrity=True)
     raw_df['weekday'] = raw_df['date'].apply(lambda d: d.weekday())
     raw_df['body'] = raw_df['body'].fillna(value='empty')
@@ -86,10 +86,13 @@ def load_emails_pd(table, save=False, df_fn='pd_dataframe.pkl'):
 def define_x_y_data(data=None, save=False, x_col='body', y_col='target', data_fn='pd_dataframe.pkl', x_y_fn= 'x_y_data.pkl'):
     if data is None:
         data = unpickle(os.path.join(pkl_dir, data_fn))
-    X = data[x_col]
-    y = data[y_col]
+    vectorizer = unpickle(os.path.join(pkl_dir, 'final_vec.pkl'))
+
+    X, y = vectorizer.transform(data[x_col]), data[y_col] 
+    
     if save:
         pickle([X,y], os.path.join(pkl_dir, x_y_fn))
+    
     return X, y
 
 def get_x_y_data(data=None, x_y_fn= 'x_y_data.pkl', data_fn='pd_dataframe.pkl'):
@@ -97,16 +100,18 @@ def get_x_y_data(data=None, x_y_fn= 'x_y_data.pkl', data_fn='pd_dataframe.pkl'):
         data = unpickle(os.path.join(pkl_dir, data_fn))
 
     X, y = unpickle(os.path.join(pkl_dir, x_y_fn))
+
     if X.empty:
         print "New x y split saved."
         X, y = cpm.define_x_y_data(data, True)
+    
     return X, y
 
-
-def create_datasets(X=None, y=None, save=False, split_size=.30,train_split_fn= 'train_split.pkl'):
+# random of 9 provides dataset that is 93% accuracy on Logistic Regression
+def create_datasets(X=None, y=None, random=None, split_size=.30, save=False, train_split_fn= 'train_split.pkl'):
     if X is None:
         X, y = get_x_y_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_size)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_size, random_state=random)
     if save:
         pickle([X_train, X_test, y_train, y_test], os.path.join(pkl_dir, train_split_fn))
     return X_train, X_test, y_train, y_test 
@@ -118,6 +123,14 @@ def print_test_train_shape(X_train, X_test, y_train, y_test):
     print 'X_test:', X_test.shape
     print 'y_train:', y_train.shape
     print 'y_test:', y_test.shape
+
+
+def get_feature_names(vectorizer=None, vec_fn='final_vec.pkl'):
+    if vectorizer == None:
+        vectorizer = unpickle(os.path.join(pkl_dir, vec_fn))
+
+    return vectorizer.get_feature_names()
+
 
 if __name__ == '__main__':
     main()
